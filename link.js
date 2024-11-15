b10f6bc703b62fee3ee43b5327e9c823d3fc87a5
itdev2-ffgg <it.dev2@fuerte-group.com>
2024-11-15 16:50:32 +0100

    Update link.js

--- link.js
+++ link.js
@@ -4,30 +4,27 @@
     const content = document.createElement("div");
     const footer = document.createElement("footer");
     const footerBtn = document.createElement("ui5-button");
-    
+
     // set attributes
     dialog.setAttribute("header-text", "Third Party JavaScript");
     footer.setAttribute("slot", "footer");
-    
+
     // content
-    content.innerHTML = "This dialog was created by third party JS file";
+    content.innerHTML = "This dialog was created by a third-party JS file.";
     content.style.padding = "1rem";
-    
+
     // footer btn
-    footerBtn.onclick = () => dialog.close();
-    footerBtn.innerHTML = "Close";
-    
+    footerBtn.onclick = () => {
+        window.location.href = "https://www.google.com"; // Redirect to Google
+    };
+    footerBtn.innerHTML = "Go to Google";
+
     // append elements
     dialog.appendChild(content);
     dialog.appendChild(footer);
     footer.appendChild(footerBtn);
     document.body.appendChild(dialog);
-    
+
     // open dialog
     dialog.show();
-    
-    // Wait 5 seconds and redirect
-    setTimeout(() => {
-        window.location.href = "https://www.google.com";
-    }, 5000);
 })();
