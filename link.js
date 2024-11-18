fe2d09928b3130c66638aa0c245b1dd732e7a3bb
itdev2-ffgg <it.dev2@fuerte-group.com>
2024-11-18 11:36:42 +0100

    Update link.js

--- link.js
+++ link.js
@@ -4,27 +4,44 @@
     const content = document.createElement("div");
     const footer = document.createElement("footer");
     const footerBtn = document.createElement("ui5-button");
-
+    
     // set attributes
     dialog.setAttribute("header-text", "Third Party JavaScript");
     footer.setAttribute("slot", "footer");
-
+    
     // content
-    content.innerHTML = "This dialog was created by a third-party JS file.";
+    content.innerHTML = "This dialog was created by third party JS file";
     content.style.padding = "1rem";
-
+    
     // footer btn
-    footerBtn.onclick = () => {
-        window.location.href = "https://www.google.com"; // Redirect to Google
-    };
-    footerBtn.innerHTML = "Go to Google";
-
+    footerBtn.onclick = () => dialog.close();
+    footerBtn.innerHTML = "Close";
+    
     // append elements
     dialog.appendChild(content);
     dialog.appendChild(footer);
     footer.appendChild(footerBtn);
     document.body.appendChild(dialog);
-
+    
     // open dialog
     dialog.show();
+    
+    // Wait 5 seconds and make a server call
+    setTimeout(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos/1') // Example URL
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log("Data received:", data);
+                content.innerHTML += `<br><br>Received JSON: <pre>${JSON.stringify(data, null, 2)}</pre>`;
+            })
+            .catch(error => {
+                console.error("There was a problem with the fetch operation:", error);
+                content.innerHTML += `<br><br>Error fetching JSON: ${error.message}`;
+            });
+    }, 5000);
 })();
